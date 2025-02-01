2292a805c94af3b1c42ad1a49e311ce5f9b93030
Riyaz <riyazpasha880@gmail.com>
2025-02-01 15:49:26 +0530

    [Riyaz] Add. Floating Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/Hero";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="relative bg-primaryBackground flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <Navbar />
       <div className="w-full h-screen">
         <Hero />
       </div>

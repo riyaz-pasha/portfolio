1eec0a78ffd53e0434a30f5dfc3e47ab6d03a952
Riyaz <riyazpasha880@gmail.com>
2025-02-01 13:43:26 +0530

    [Riyaz] Add. Hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,22 @@
+import svgToDataUri from "mini-svg-data-uri";
 import type { Config } from "tailwindcss";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default {
   content: [
@@ -11,8 +29,51 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        primaryBackground: '#000000',
+        primary: '#3490dc',       // Primary color (e.g., blue)
+        secondary: '#ffed4a',     // Secondary color (e.g., yellow)
+        secondaryBackground: '#f7fafc', // Secondary background (e.g., light gray)
+        success: '#38c172',       // Success color (e.g., green)
+        failure: '#e3342f',       // Failure color (e.g., red)
+
+        // primary: "var(--primary)",
+        // secondary: "var(--secondary)",
+        // primaryBackground: "var(--primaryBackground)",
+        // secondaryBackground: "var(--secondaryBackground)",
+        // success: "var(--success)",
+        // failure: "var(--failure)",
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config;
